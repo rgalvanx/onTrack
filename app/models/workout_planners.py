e317d63083e6b6e3051965aae3689b49223ceabf
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-20 12:58:51 -0600

    added workout_planner models

---
+++ app/models/workout_planners.py
@@ -0,0 +1,27 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+
+class WorkoutPlan(db.Model):
+    __tablename__ = 'workout_plans'
+
+    if environment == 'production':
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String, nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey(add_prefix_for_prod('users.id'),
+        ondelete='CASCADE'),
+        nullable=False
+    )
+
+    user = db.relationship('User', back_populates='workout_plans')
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'category': self.category,
+            'content': self.content,
+            'user_id': self.user_id
+        }
