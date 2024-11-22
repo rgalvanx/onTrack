cd2467999bfb96c074c82ca8a71e51129cc38012
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-22 15:06:13 -0600

    successfully deleted workout planners

---
+++ react-vite/src/components/Workouts/DeleteWorkout.jsx
@@ -0,0 +1,30 @@
+import { useParams, useNavigate } from "react-router-dom";
+import { useModal } from "../../context/Modal";
+import { deletePlanThunk } from "../../store/workoutPlan";
+import { useDispatch } from "react-redux";
+
+const DeleteWorkout = ({ workout }) => {
+    const { workoutId } = useParams()
+    const dispatch = useDispatch()
+    const { closeModal } = useModal()
+    const navigate = useNavigate()
+
+    const deletePlan = () => {
+        dispatch(deletePlanThunk( workoutId, ))
+        closeModal()
+        navigate('/')
+    }
+
+    return (
+        <div className="delete-workout-modal">
+            <h1 className="delete-title">Are you sure you want to delete your {workout?.category} workout?</h1>
+            <div className="options">
+                <button className="keep" onClick={closeModal}>No (Keep Plan)</button>
+                <button className="delete" onClick={deletePlan}>Yes (Delete Plan)</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default DeleteWorkout
--- react-vite/src/components/Workouts/WorkoutsById.jsx
+++ react-vite/src/components/Workouts/WorkoutsById.jsx
@@ -3,7 +3,8 @@ import { loadPlansThunk } from "../../store/workoutPlan";
 import { deletePlanThunk } from "../../store/workoutPlan";
 import { useParams, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import OpenModalMenuItem from "../Navigation/OpenModalMenuItem";
+import OpenModalButton from "../Navigation/OpenModalButton";
+import DeleteWorkout from "./DeleteWorkout";
 import './Workouts.css'
 
 export default function WorkoutsById() {
@@ -11,11 +12,18 @@ export default function WorkoutsById() {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const workouts = useSelector((state) => state.workouts[workoutId])
-    console.log(workouts)
+    const user = useSelector((state) => state.session.user)
+    const correctUser = user.id === workouts?.user_id
+    console.log(correctUser)
 
     useEffect(() => {
         dispatch(loadPlansThunk(workoutId))
     }, [dispatch])
+
+    const handleDelete = () => {
+        dispatch(deletePlanThunk(workoutId))
+        navigate('/')
+    }
 
     if(!workouts) return (
         <h1>No Projects Found!</h1>
@@ -27,12 +35,16 @@ export default function WorkoutsById() {
                 <h1 className="header-category">{workouts?.category}</h1>
                 <h2 className="header-name">By {workouts?.username}</h2>
             </div>
-            <div className="content">
+            <div className="content" style={{paddingBottom: '40px'}}>
                 <div className="workout-container">
                     <h3 className="workout-content">{workouts?.content}</h3>
                 </div>
             </div>
-            {}
+                {correctUser && (
+                    <div>
+                        <OpenModalButton className="delete-button" itemText='Delete Workout' modalComponent={<DeleteWorkout workout={workouts}/>}>Delete Workout</OpenModalButton>
+                    </div>
+                )}
         </>
     )
 }
