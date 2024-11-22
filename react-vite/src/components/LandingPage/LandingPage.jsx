b6f76f4be3b0da2c4a3ea636a6295139621371f6
Ruben G <159964698+rgalvanx@users.noreply.github.com>
2024-11-21 18:32:22 -0600

    edited landing page

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,12 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadPlansThunk } from "../../store/workoutPlan";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
 
 export default function LandingPage() {
     const dispatch = useDispatch()
-    // const navigate = useNavigate()
+    const navigate = useNavigate()
     const plans = useSelector((state) => Object.values(state.workouts))
     console.log(plans)
 
@@ -16,16 +16,29 @@ export default function LandingPage() {
     }, [dispatch])
 
     return (
-        <div className="landing-page">
-            <button>Create a Workout Plan!</button>
-            <ul className="workout-list">
-
+        <div
+        className="landing-page"
+        style={{marginTop: '50px'}}>
+            <button
+            style={{marginLeft: '50px'}}
+            onClick={(e) => {
+                e.preventDefault()
+                navigate(`/workout_plans/create`)
+            }}>Create a Workout Plan!
+            </button>
+            <ul
+            className="workout-list">
             {plans.length > 0 ? (
                 <>
                     {plans.map((workout) => (
-                        <div className="workout-box"key={workout.id}>
-                            <h1>{workout.category}</h1>
-                            <h2>{workout.content}</h2>
+                        <div className="workout-box"key={workout.id}
+                        onClick={(e) => {
+                            e.preventDefault()
+                            navigate(`/workout_plans/${workout.id}`)
+                        }}>
+                            <h1 className="workout category">{workout.category}</h1>
+                            {/* <h2 >{workout.content}</h2> */}
+                            <h2 className="workout username">by {workout.username}</h2>
                         </div>
                     ))}
                 </>
